a452c9668aacf1ec4cd5cf3c509031aa707032b2
Yusuf Eren Çelebi <erencelebi@marun.edu.tr>
2026-06-01 08:27:23 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from prometheus_fastapi_instrumentator import Instrumentator
 from . import models
 from src.services.s3 import upload_file
 
-# Demo Mesajı
+# Demo Mesajı 2
 # DB bağlantı URL'si (Çevresel değişkenden veya varsayılan SQLite)
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./todos.db")
 
